ac2ea866dcc6a329056029b0c779a547dc4f2bed
Michael Dekker <git@michaeldekker.nl>
2017-07-09 16:17:07 +0200

    Bump to 1.2.2

--- stripe.php
+++ stripe.php
@@ -106,7 +106,7 @@ class Stripe extends PaymentModule
     {
         $this->name = 'stripe';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2.1';
+        $this->version = '1.2.2';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
 
