ade5656216c6a6470239a2b33b9ae8cbeae00814
Michael Dekker <git@michaeldekker.nl>
2017-08-31 16:10:37 +0200

    Fix tests

--- tests/bootstrap.no_autoload.php
+++ tests/bootstrap.no_autoload.php
@@ -3,7 +3,4 @@
 // Module composer
 require_once __DIR__ . '/../vendor/autoload.php';
 
-// thirty bees composer
-require_once __DIR__ . '/../../../vendor/autoload.php';
-
 require_once __DIR__ . '/TestCase.php';
