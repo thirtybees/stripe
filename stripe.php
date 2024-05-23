827c70891f504bef9b0bcd5cd32cc2b0938ad574
Petr Hučík <petr@getdatakick.com>
2024-05-23 14:21:22 +0200

    Release version 1.9.2

--- stripe.php
+++ stripe.php
@@ -114,7 +114,7 @@ class Stripe extends PaymentModule
     {
         $this->name = 'stripe';
         $this->tab = 'payments_gateways';
-        $this->version = '1.9.1';
+        $this->version = '1.9.2';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
